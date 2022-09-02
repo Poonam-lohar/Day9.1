fb6d8121176811ef817ec27635e830ee0fe99651
Your Name <you@example.com>
2022-09-02 11:38:35 +0530

    logic for Armstrong number

---
+++ src/ArmstrongNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int count=0,a,b,c,sum=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        a=n;
+        c=n;
+        while (a>0)
+        {
+            a=a/10;
+            count++;
+        }
+        while (n>0) {
+            b = n % 10;
+            sum = (int) (sum + Math.pow(b, count));
+            n = n / 10;
+        }
+            if (sum==c) {
+                System.out.println("Number is Armstrong");
+            }
+            else {
+                System.out.println("Number is not Armstrong");
+            }
+        }
+    }
